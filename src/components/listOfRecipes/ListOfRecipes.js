8f7123c0cefe734c6c201bd163723e527f5fa49e
James Morrissey <jammim19@gmail.com>
2024-08-06 20:10:25 +0100

    Added some badges tw classes for the recipes properties

--- src/components/listOfRecipes/ListOfRecipes.js
+++ src/components/listOfRecipes/ListOfRecipes.js
@@ -23,24 +23,38 @@ function ListOfRecipes() {
           return (
             <li
               key={id}
-              class="p-1 max-w-md mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 m-10"
+              className="p-1 max-w-md mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 m-10"
             >
               <div>
                 <img alt={recipe} src={image}></img>
               </div>
               <div>
-                <h4 class="text-xl font-medium text-black">{recipe}</h4>
-                <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+                <h4 className="text-xl font-medium text-black">{recipe}</h4>
+                <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
                   Difficulty : {difficulty}
-                </span>{" "}
-                |<span>Serves: {serving} people</span> |
-                <span>Prep time : {prep_time_in_minutes} minutes</span> |
-                <span>Cook time : {cook_time_in_minutes} minutes</span> |
-                <span>Calories : {calories} </span> |
-                <span>Fat : {fat_in_grams} grams</span> |
-                <span>Carbs : {carbohydrates_in_grams} grams</span> |
-                <span>Protein : {protein_in_grams} grams</span> |
-                <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Serves: {serving} people
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Prep time : {prep_time_in_minutes} minutes
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Cook time : {cook_time_in_minutes} minutes
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Calories : {calories}
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Fat : {fat_in_grams} grams
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Carbs : {carbohydrates_in_grams} grams
+                </span>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                  Protein : {protein_in_grams} grams
+                </span>
+                <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                   {category.category}
                 </span>
               </div>
